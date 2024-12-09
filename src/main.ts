dea08a6611e01df7d47ec0acc9ab49cc4f370468
Artem Soloviov <artsolo0408@gmail.com>
2024-12-09 15:11:55 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './assets/main.css';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+import { definePreset } from '@primevue/themes';
+import { palette } from '@primevue/themes';
+
+const app = createApp(App);
+
+const customPreset = definePreset(Aura, {
+  semantic: {
+    primary: palette('#007aff'),
+  },
+});
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: customPreset,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+});
+
+app.mount('#app');
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Button, InputText, Password, FloatLabel, Checkbox } from 'primevue';
+import DarkLogo from '@/components/icons/DarkLogo.vue';
+
+const login = () => {
+  alert('logged in');
+};
+</script>
+
+<template>
+  <main class="h-screen flex items-center justify-center">
+    <form class="flex flex-col w-56 gap-5" @submit.prevent="login()">
+      <div class="flex justify-center">
+        <DarkLogo />
+      </div>
+
+      <FloatLabel class="mt-5" variant="in">
+        <InputText class="w-full" id="username" />
+        <label for="password">Ім'я користувача</label>
+      </FloatLabel>
+
+      <FloatLabel variant="in">
+        <Password class="w-full" id="password" :feedback="false" toggleMask />
+        <label for="password">Пароль</label>
+      </FloatLabel>
+
+      <div class="flex items-center gap-2">
+        <Checkbox id="rememberMe" binary />
+        <label for="rememberMe">Запам'ятати дані</label>
+      </div>
+
+      <div class="flex justify-center">
+        <Button type="submit" label="Увійти" rounded class="px-5" />
+      </div>
+    </form>
+  </main>
+</template>
+
+<style>
+#password input {
+  width: 100%;
+}
+</style>
