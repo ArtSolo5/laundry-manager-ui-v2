d465083439f5373d6e7f46ef8be99790a92f51be
Artem Soloviov <artsolo0408@gmail.com>
2024-12-11 08:24:25 +0200

    base views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DashboardView from '../views/DashboardView.vue';
 import LoginView from '@/views/LoginView.vue';
+import ArrivalsView from '@/views/ArrivalsView.vue';
+import WashesView from '@/views/WashesView.vue';
+import ReportView from '@/views/ReportView.vue';
+import HandbookView from '@/views/HandbookView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +18,26 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/arrivals',
+      name: 'arrivals',
+      component: ArrivalsView,
+    },
+    {
+      path: '/washes',
+      name: 'washes',
+      component: WashesView,
+    },
+    {
+      path: '/report',
+      name: 'report',
+      component: ReportView,
+    },
+    {
+      path: '/handbook',
+      name: 'handbook',
+      component: HandbookView,
     },
   ],
 });
---
+++ src/views/WashesView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <h1>Washes</h1>
+  </section>
+</template>
