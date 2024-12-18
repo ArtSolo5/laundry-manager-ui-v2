faab3540e7c5361489f4a9a960759c752150fcfe
Artem Soloviov <artsolo0408@gmail.com>
2024-12-18 16:47:59 +0200

    active menu item

--- src/components/common/TheSidebar.vue
+++ src/components/common/TheSidebar.vue
@@ -5,6 +5,9 @@ import { PrimeIcons } from '@primevue/core/api';
 import permissions from '@/permissions/sidebar';
 import { computed } from 'vue';
 import { useAuthStore } from '@/stores/auth';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const authStore = useAuthStore();
 
@@ -44,6 +47,10 @@ const items: MenuItem[] = [
 const allowedMunuItems = computed(() => {
   return items.filter((i) => authStore.isAllowed(i.permissions));
 });
+
+const isActiveRoute = (routePath: string): boolean => {
+  return route.path === routePath;
+};
 </script>
 
 <template>
@@ -51,7 +58,7 @@ const allowedMunuItems = computed(() => {
     <TieredMenu class="border-r border-0 rounded-none" :model="allowedMunuItems">
       <template #item="{ item, props }">
         <router-link v-slot="{ href, navigate }" :to="item.route" custom>
-          <a :href="href" v-bind="props.action" @click="navigate">
+          <a :href="href" v-bind="props.action" @click="navigate" :class="{'p-menuitem-active': isActiveRoute(item.route)}">
             <span :class="item.icon" />
             <span class="ml-2">{{ item.label }}</span>
           </a>
@@ -64,5 +71,10 @@ const allowedMunuItems = computed(() => {
 <style>
 aside div {
   height: 100%;
+}
+
+.p-menuitem-active {
+  background-color: #F1F5F9;
+  border-radius: 4px;
 }
 </style>
